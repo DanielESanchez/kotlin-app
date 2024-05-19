b4060dbf20f2dd41d559f39f9c3280e28fafc44c
Daniel Sanchez <danielsanczuniga@gmail.com>
2024-05-19 14:27:11 -0600

    Finished home screen

--- app/src/main/java/com/myneflow/davisi/ui/home/component/SocialProjects.kt
+++ app/src/main/java/com/myneflow/davisi/ui/home/component/SocialProjects.kt
@@ -37,13 +37,16 @@ import com.myneflow.davisi.R
 
 @Composable
 fun Carousel(cardImages: List<Int>) {
+    val pagerState2 = rememberPagerState(pageCount = { cardImages.size })
     val pagerState = rememberPagerState(pageCount = { cardImages.size })
 
-    Box(){
+    Box() {
         val url = "https://www.undp.org/sites/g/files/zskgke326/files/2023-09/playas.jpg"
-        Text(text = "Proyectos Sociales", modifier = Modifier
-            .padding(start = 16.dp)
-            .padding(top = 220.dp), color = Color.Black, style = TextStyle(fontSize = 20.sp))
+        Text(
+            text = "Proyectos Sociales", modifier = Modifier
+                .padding(start = 16.dp)
+                .padding(top = 220.dp), color = Color.Black, style = TextStyle(fontSize = 20.sp)
+        )
         HorizontalPager(
             state = pagerState,
             modifier = Modifier
@@ -58,9 +61,11 @@ fun Carousel(cardImages: List<Int>) {
                     .background(Color.Transparent),
                 colors = CardDefaults.cardColors(containerColor = Color.Transparent)
             ) {
-                Column(modifier = Modifier
-                    .fillMaxWidth()
-                    .background(Color.Transparent)) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(Color.Transparent)
+                ) {
                     Image(
                         painter = rememberImagePainter(data = url),
                         contentDescription = null,
@@ -70,17 +75,41 @@ fun Carousel(cardImages: List<Int>) {
                             .fillMaxWidth()
                     )
                     Spacer(modifier = Modifier.height(2.dp))
-                    Text(text = "Universidad Tecnologica UTEC", modifier = Modifier.padding(2.dp), style = TextStyle(fontSize = 12.sp), color = Color.Gray)
-                    Text(text = "\"Proyecto universitario para causa social\"",
+                    Text(
+                        text = "Universidad Tecnologica UTEC",
                         modifier = Modifier.padding(2.dp),
-                        style = TextStyle(fontSize = 12.sp, fontWeight = FontWeight.Bold), color = Color.Black)
+                        style = TextStyle(fontSize = 12.sp),
+                        color = Color.Gray
+                    )
+                    Text(
+                        text = "\"Proyecto universitario para causa social\"",
+                        modifier = Modifier.padding(2.dp),
+                        style = TextStyle(fontSize = 12.sp, fontWeight = FontWeight.Bold),
+                        color = Color.Black
+                    )
                     Row {
-                        Icon(painter = painterResource(id = R.drawable.icon_people), contentDescription = "" )
-                        Text(text = "1.5k colaboradores", modifier = Modifier.padding(2.dp), style = TextStyle(fontSize = 12.sp), color = Color.Gray)
-                        Icon(painter = painterResource(id = R.drawable.icon_goal), contentDescription = "", modifier = Modifier.height(16.dp).width(16.dp).padding(start = 4.dp) )
+                        Icon(
+                            painter = painterResource(id = R.drawable.icon_people),
+                            contentDescription = ""
+                        )
+                        Text(
+                            text = "1.5k colaboradores",
+                            modifier = Modifier.padding(2.dp),
+                            style = TextStyle(fontSize = 12.sp),
+                            color = Color.Gray
+                        )
+                        Icon(
+                            painter = painterResource(id = R.drawable.icon_goal),
+                            contentDescription = "",
+                            modifier = Modifier.height(16.dp).width(16.dp).padding(start = 4.dp)
+                        )
                         AndroidView(
                             factory = { context ->
-                                ProgressBar(context, null, android.R.attr.progressBarStyleHorizontal).apply {
+                                ProgressBar(
+                                    context,
+                                    null,
+                                    android.R.attr.progressBarStyleHorizontal
+                                ).apply {
                                     progress = (0.7f * 100).toInt()
                                 }
                             },
@@ -92,6 +121,36 @@ fun Carousel(cardImages: List<Int>) {
                 }
             }
         }
-    }
+        Text(
+            text = "Canjea tus puntos ISI", modifier = Modifier
+                .padding(start = 16.dp)
+                .padding(top = 450.dp), color = Color.Black, style = TextStyle(fontSize = 20.sp)
+        )
+        HorizontalPager(
+            state = pagerState2,
+            modifier = Modifier
+                .padding(16.dp)
+                .padding(top = 460.dp),
+            pageSize = PageSize.Fixed(150.dp)
+        ) { page ->
+            Card(
+                modifier = Modifier
+                    .height(220.dp)
+                    .padding(8.dp)
+                    .background(Color.Transparent),
+                colors = CardDefaults.cardColors(containerColor = Color.Transparent)
+            ) {
+                    Image(
+                        painter = painterResource(R.drawable.combos),
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier
+                            .height(70.dp)
+                            .width(300.dp)
+                    )
+            }
 
+        }
+
+    }
 }
